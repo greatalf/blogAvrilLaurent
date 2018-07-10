f9c32fc0d27ddd5c05546283b072ef37d0a6a55d
Greatalf <avril.laurent974@yahoo.fr>
2018-07-10 19:13:49 +0400

    commit index

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 
 require 'vendor/autoload.php';
-require 'app/Router.php';
+// require '../app/.heroku/php/lib/php';
+require 'app/controllers/Router.php';
+// use Laurent\App\Controllers\Router;
 
 $router = new Router;
 $router->routeReq();
